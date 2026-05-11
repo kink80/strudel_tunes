220387d333b03a059436da6cc4026ba15ac83d61
Slavek Tecl <slavomir.tecl@gmail.com>
2026-05-11 08:09:00 +0200

    Update dark.js

--- dark.js
+++ dark.js
@@ -1,25 +1,35 @@
-$: note("[a1 b1 c2 e2]*2  [a1 b1 c2 f2]*2")
-  .pan(sine.fast(2))
-  .gain(slider(0.3701, 0.2, 0.5))
+_$: note("[a1 b1 c2 e2]*2  [a1 b1 c2 f2]*2")
+  .pan(sine.slow(2))
+  .room(0.3)
+  .gain(slider(0.27, 0.1, 0.5))
   .orbit("2")
   .vst("bass")
 
-// hass - odin2, preset mine
+// odin preset
 
-_$: n("0 2 4 6 4 2").scale("A:minor").vst("bass")
-
-$: note("[a1 a1]")
-  .gain(3.0)
+$: note("[a2 a2 a2 a2 b2 b2 b2 b2 c2 c2 c2 c2 b2 b2 b2 b2]/2")
+  .gain(2.4)
   .pan(sine.slow(5))
   .vst("d1")
 
 // d1 - dexed, preset 26.
 
-$: note("[a3 a3]")
-   .vst("d2")    
+_$: note("[a1 a1 a1 a1 a1]")
+   .gain(1.2)
+   .phaser(2)
+   .vst("d2")     
 
 // d2 - dexed, preset 10
 
-$: s("bd*8").bank("ddm110").gain(1.2)
-$: s("~ [rim, sd:<2 3>]").room("<0 .2>")
-$: n("[0 <1 3>]*<2!3 4>").s("hh")
+_$: note("[a3 g#3 g3 f#3]/4")
+  .delay(0.8)
+  .delaytime(2/3)
+  .delayfb(0.7) 
+  .room(0.8)
+  .vst("d3")
+
+// d3 - dexed, preset 16/17?
+
+$: s("bd*8").bank("ddm110").gain(0.2)
+$: s("~ [rim, sd:<2 3>]").room("<0 .2>").gain(0.2)
+$: n("[0 <1 3>]*<2!3 4>").s("hh").gain(0.2)
