1a2752a2de040bb8820de9e4a305da72671980e1
Slavek Tecl <slavomir.tecl@gmail.com>
2026-06-10 17:03:40 +0200

    Update chant.js

--- chant.js
+++ chant.js
@@ -1,18 +1,17 @@
-// midiclockin('BOSS_RC-5')
-
 setCpm(110/4)
 
+samples({
+'arab': '/arab.wav',
+}, 'http://localhost:5432')
 
-$: n("0 1 [4 2] [3 3]")
+$: s("arab")
+  .scrub("{.26@16}%8")
+  .gain(.1)
+
+$: n("0 1 [2 2] [3 3]")
   .sound("jazz")
   .room(0.4)
   .gain(0.3)
 
-// surget xt - robotic saw = init saw, scena -2 pitch
-// fx1 - flanger
-// fx2 - freq shift, building robot monkeys
-
 $: note("<g3 ~ g4 g#4>*8")
    .vst("d2")
-   .room(0.8)
-   .pan(sine.fast(4))
