8cd9021847616bddfd7d20f81f3831badf2aa5c7
Slavek Tecl <slavomir.tecl@gmail.com>
2026-06-11 07:09:23 +0200

    Update chant.js

--- chant.js
+++ chant.js
@@ -1,17 +1,23 @@
+// midiclockin('BOSS_RC-5')
+
 setCpm(110/4)
 
 samples({
 'arab': '/arab.wav',
 }, 'http://localhost:5432')
 
-$: s("arab")
-  .scrub("{.26@16}%8")
+_$: s("arab")
+  .scrub("{.26@32}%8")
   .gain(.1)
 
-$: n("0 1 [2 2] [3 3]")
+$: n("0 1 [~ 5] [~ 5]")
   .sound("jazz")
   .room(0.4)
   .gain(0.3)
 
 $: note("<g3 ~ g4 g#4>*8")
    .vst("d2")
+
+_$: note("<g2 ~ g2 ~>*4")
+   .vst("d1")
+   .gain(1.2)
