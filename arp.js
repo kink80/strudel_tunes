f7c67466317a915ac0099de759ec618d06a08d74
Slavek Tecl <slavomir.tecl@gmail.com>
2026-06-22 20:49:31 +0200

    Update arp.js

--- arp.js
+++ arp.js
@@ -1,28 +1,34 @@
 
 
-$: n("[0 2 4 7]*4")
+$: n("[0 2 4 7 4 7 6 4]*2")
   .scale("[G:minor G:minor G:minor G:major]/4")
+  .swingBy(1/3, 4)
   .gain(0.2)
   .vst("arp")
 
-
-bend = slider(0.5, 0, 1)
-
-
 $: n("[0 0]")
    .scale("[G:minor G:minor G:minor G:major]/4")
-   .midibend(bend.mul(2).sub(1))
-   .gain(0.2)
+   .gain(0.7)
+   .pan(sine.slow(4))
    .vst("drone")
-
 
 $: pure(1)
    .fast(30)
    .ccn(102)
-   .ccv(cosine.range(0.3, 0.5).slow(4))
+   .ccv(sine.range(0.3, 0.5).slow(4))
    .vst("drone")
 
-$: s(seq("bd", "hh", "bd", "cp cp".delay(.3)))
+$: s("bd [bd bd] bd [bd bd]")
+   .bank('tr909')
+   .room(1.2)
+   .gain(0.2)
+
+$: s("[oh hh] [~ hh] ~ hh")
+   .bank('tr909')
+   .room(1.2)
+   .gain(0.2)
+
+$: s("~ ~ [cp cp] ~".delay(.3))
    .bank('tr909')
    .room(1.2)
    .gain(0.4)
