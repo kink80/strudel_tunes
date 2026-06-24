3cab503ec5d02f417a75accc4bc5c5657f03d7be
Slavek Tecl <slavomir.tecl@gmail.com>
2026-06-24 07:52:02 +0200

    Update arp.js

--- arp.js
+++ arp.js
@@ -1,11 +1,19 @@
 
 
-$: n("[0 2 4 7 4 7 6 4]*2")
+_$: n("[0 2 4 7 4 7 6 4]*2")
   .scale("[G:minor G:minor G:minor G:major Eb:lydian Eb:lydian D:phrygian D:phrygian]/8")
   .scramble(4)
   //.rev()
   .gain(0.2)
   .vst("arp")
+
+
+$: n("0 ~ 4 ~ 7 ~ [12 7] ~")
+  .scale("[G:minor G:minor G:minor G:major Eb:lydian Eb:lydian D:phrygian D:phrygian]/8")
+  .rev()
+  .gain(0.2)
+  .vst("cray")
+
 
 $: pure(1)
    .fast(30)
@@ -18,7 +26,7 @@ _$: n("[[0 0] ~ 0]")
    .scale("[G:minor G:minor G:minor G:major Eb:lydian Eb:lydian D:phrygian D:phrygian]/8")
    .vst("bass")
 
-$: n("[[0 7] [0 7] [2 7] [4 7]]")
+_$: n("[[0 7] [0 7] [2 7] [4 7]]")
    .scale("[G:minor G:minor G:minor G:major Eb:lydian Eb:lydian D:phrygian D:phrygian]/8")
    .vst("bass")
 
@@ -29,7 +37,7 @@ $: pure(1)
    .ccv(sine.range(0.0, 1.0).slow(4))
    .vst("bass")
 
-$: n("[0 0]")
+_$: n("[0 0]")
    .scale("[G:minor G:minor G:minor G:major Eb:lydian Eb:lydian D:phrygian D:phrygian]/8")
    .gain(0.7)
    .pan(sine.slow(4))
@@ -41,7 +49,7 @@ _$: pure(1)
    .ccv(sine.range(0.47, 0.53).slow(8))
    .vst("drone")
 
-$: s("bd [bd bd] bd [bd bd]")
+_$: s("bd [bd bd] bd [bd bd]")
    .bank('tr909')
    .room(1.2)
    .gain(0.5)
