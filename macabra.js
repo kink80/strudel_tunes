b2cf8c8052b23ecd263df1baddb9d0b983795ca0
Slavek Tecl <slavomir.tecl@gmail.com>
2026-08-04 07:38:25 +0200

    Update macabra.js

--- macabra.js
+++ macabra.js
@@ -4,6 +4,28 @@ let chords = chord(`<
 Em9 D6 CM7 D6
 >`)
 
+let chordsBridge = chord(`
+<
+Am9 Bm7 CM7 B7b13
+>
+`)
+
+chordsBridge = chords
+
+
+_$bridgeChords: chordsBridge
+    .anchor("e6")
+    .voicing()
+    .struct("~ 1 ~ 1")
+    .vst("pad3")
+
+  
+_$bridgeBass: 
+    n("<[~ 0 ~ 4 ~ 2 1 ~] [~ 0 ~ 4 ~ 2 1 ~] [~ 0 ~ 4 ~ 2 1 ~] [4 3 2 1 0 -1 ~ ~]>")
+    .set(chordsBridge)
+    .mode("root:e3")
+    .voicing()  
+    .vst("bass")
 
 $: n("0@4 0@2 1@4 1@2")
   .set(chords)
@@ -26,12 +48,12 @@ $: n("[0 2 4 7 4 7 6 4]*2")
   .set(chords)
    .mode("root:e3")
    .voicing()
-  .rev()
+  //.rev()
   .gain(0.2)
   .vst("pad2")
 
 
-$: note("e6(<8>, 16, <0>)")
+_$: note("e6(<8>, 16, <0>)")
    .vst("signal")
 
 
@@ -45,3 +67,5 @@ $: note("e4(<3 5 6 3>, 8, <0 0 2 0>)")
 $: note("<e4 e4 e4 e4>*4")
    .vst("hat")
 
+$: s("~ cp ~ [cp cp]")
+   .gain(0.1)
